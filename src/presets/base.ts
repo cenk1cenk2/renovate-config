4eabf20f794b1f6ad2a0114cd3c5a1e3888212d8
Cenk Kılıç <cenk@kilic.dev>
2020-07-21 18:15:36 +0200

    fix(pr-request): pending status to immediate

--- src/presets/base.ts
+++ src/presets/base.ts
@@ -1,6 +1,5 @@
 import { TIMEZONE } from '@constants/constants'
 import { SCHEDULE } from '@constants/renovate'
-import { assignees } from '@constants/users'
 import { createPreset } from '@lib/preset-factory'
 
 export default createPreset({
@@ -10,15 +9,13 @@ export default createPreset({
   semanticCommits: true,
   semanticCommitType: 'fix',
   rangeStrategy: 'update-lockfile',
-  assignees,
   lockFileMaintenance: {
     enabled: true,
-    automerge: true,
     schedule: [ SCHEDULE.WEEKLY ]
   },
   major: {
     stabilityDays: 3
   },
   schedule: [ SCHEDULE.DAILY ],
-  prCreation: 'not-pending'
+  prCreation: 'immediate'
 })
