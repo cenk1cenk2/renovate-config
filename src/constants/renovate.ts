cca9f2563f64f9331029edd2219a671e99651c9b
Cenk Kılıç <cenk@kilic.dev>
2020-07-21 16:44:16 +0200

    fix: disable pin dependencies

--- src/constants/renovate.ts
+++ src/constants/renovate.ts
@@ -4,5 +4,6 @@ export enum SCHEDULE {
   EVERY_MONDAY = 'before 1am on Monday',
   WEEKDAYS = 'every weekday',
   MONTHLY = 'before 1am on the first day of the month',
-  DAILY = 'before 1am'
+  DAILY = 'before 1am',
+  ANY = 'at any time'
 }
--- src/presets/base.ts
+++ src/presets/base.ts
@@ -1,4 +1,5 @@
 import { TIMEZONE } from '@constants/constants'
+import { SCHEDULE } from '@constants/renovate'
 import { createPreset } from '@lib/preset-factory'
 
 export default createPreset({
@@ -7,11 +8,14 @@ export default createPreset({
   timezone: TIMEZONE,
   semanticCommits: true,
   semanticCommitType: 'fix',
+  rangeStrategy: 'update-lockfile',
   lockFileMaintenance: {
-    enabled: true
+    enabled: true,
+    schedule: [ SCHEDULE.EVERY_MONDAY ]
   },
   major: {
     stabilityDays: 3
   },
+  schedule: [ SCHEDULE.ANY ],
   prCreation: 'not-pending'
 })
