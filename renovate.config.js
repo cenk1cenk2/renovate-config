e477509bac37b6ac5ba7105c0b07138b1e2afccc
Cenk Kılıç <cenk@kilic.dev>
2020-07-21 18:32:36 +0200

    ci(repos): changed places of repos

--- renovate.config.js
+++ renovate.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   repositories: [
     'cenk1cenk2/renovate',
-    'cenk1cenk2/boilerplate-oclif',
-    'cenk1cenk2/eslint-config'
+    'cenk1cenk2/eslint-config',
+    'cenk1cenk2/boilerplate-oclif'
   ]
 }
