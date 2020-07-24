6e6570b908113998f51bce706d75a113df0342f0
Cenk Kılıç <cenk@kilic.dev>
2020-07-24 02:42:18 +0200

    chore: disable renovate action for now

--- renovate.config.js
+++ renovate.config.js
@@ -3,7 +3,7 @@ module.exports = {
   gitAuthor: 'renovate-bot <renovate@kilic.dev>',
   enabledManagers: [ 'github-actions', 'ansible', 'ansible-galaxy', 'docker-compose', 'dockerfile', 'droneci', 'git-submodules', 'gomod', 'kubernetes', 'npm', 'nvm' ],
   logLevel: 'info',
-  masterIssueTitle: '[renovate-bot] Update Dependencies',
+  dependencyDashboardTitle: '[renovate-bot] Update Dependencies',
   onboarding: true,
   onboardingPrTitle: '[renovate-bot] Configure',
   onboardingConfig: {
