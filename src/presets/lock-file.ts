0307ee78a59c65732a99725cdd12a772825f82a7
Cenk Kılıç <cenk@kilic.dev>
2020-07-26 20:03:22 +0200

    fix(lock-file): added skip ci to lock file

--- src/presets/lock-file.ts
+++ src/presets/lock-file.ts
@@ -5,6 +5,7 @@ export default createPreset({
   lockFileMaintenance: {
     enabled: true,
     automerge: true,
+    commitMessageSuffix: '[skip ci]',
     labels: [ 'renovate', 'lock' ],
     schedule: [ SCHEDULE.TWICE_A_DAY ]
   }
