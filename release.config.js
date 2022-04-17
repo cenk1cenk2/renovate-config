724251b60b70b75793ea89c9cf8647b3c8abcd5a
Cenk Kılıç <cenk@kilic.dev>
2022-04-17 20:57:45 +0200

    ci: update release configuration

--- release.config.js
+++ release.config.js
@@ -14,13 +14,17 @@ module.exports = {
       prerelease: true
     }
   ],
-  verifyConditions: [ '@semantic-release/changelog', '@semantic-release/git' ],
-  prepare: [
-    '@semantic-release/changelog',
-    {
-      path: '@semantic-release/git',
-      assets: [ 'CHANGELOG.md', 'README.md', 'yarn.lock', 'package.json' ],
-      message: 'chore(release): <%= nextRelease.version %> - <%= new Date().toISOString().slice(0,10).replace(/-/g,\'\') %> [skip ci]\n\n<%= nextRelease.notes %>'
-    }
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [ 'CHANGELOG.md', 'README.md', 'yarn.lock', 'package.json' ],
+        message: 'chore(release): <%= nextRelease.version %> - <%= new Date().toISOString().slice(0,10).replace(/-/g,\'\') %> [skip ci]\n\n<%= nextRelease.notes %>'
+      }
+    ],
+    '@semantic-release/gitlab'
   ]
 }
