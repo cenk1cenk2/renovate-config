64b4234b8ccbb7f22eb7c1686b56aeb3cb6ae4cb
Cenk Kılıç <cenk@kilic.dev>
2022-04-17 21:10:26 +0200

    fix: add more semantic release configuration

--- release.config.js
+++ release.config.js
@@ -18,12 +18,13 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
+    '@semantic-release/changelog',
     [
-      '@semantic-release/git',
+      ('@semantic-release/git',
       {
         assets: [ 'CHANGELOG.md', 'README.md', 'yarn.lock', 'package.json' ],
         message: 'chore(release): <%= nextRelease.version %> - <%= new Date().toISOString().slice(0,10).replace(/-/g,\'\') %> [skip ci]\n\n<%= nextRelease.notes %>'
-      }
+      })
     ],
     '@semantic-release/gitlab'
   ]
