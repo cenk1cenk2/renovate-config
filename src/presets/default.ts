0ea110dae783dbf071c07e08d7648fc3c459ec9a
Cenk Kılıç <cenk@kilic.dev>
2023-12-09 17:59:27 +0100

    fix: remove from defautl

--- src/presets/default.ts
+++ src/presets/default.ts
@@ -5,7 +5,6 @@ export default createPreset({
   extends: [
     createScope(Preset.BASE),
     createScope(Preset.LOCK_FILE),
-    createScope(Preset.NO_TESTS),
     createScope(Preset.GROUP_MINOR_DEPENDENCIES),
     createScope(Preset.GROUP_DEV_DEPENDENCIES),
     createScope(Preset.GROUP_PEER_DEPENDENCIES)
