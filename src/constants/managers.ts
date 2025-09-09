ae5251a3ca8c849d9ff053c5abeb123534dd041c
Cenk Kılıç <cenk@kilic.dev>
2025-09-09 21:47:52 +0200

    fix: add helm-values as a manager

--- src/constants/managers.ts
+++ src/constants/managers.ts
@@ -2,6 +2,7 @@ export enum Managers {
   ANSIBLE_GALAXY = 'ansible-galaxy',
   GO = 'gomod',
   HELM = 'helm',
+  HELM_VALUES = 'helm-values',
   KUSTOMIZE = 'kustomize',
   NODE = 'npm',
   PYTHON_PEP621 = 'pep621',
--- src/presets/managers/helm/manager.ts
+++ src/presets/managers/helm/manager.ts
@@ -2,5 +2,5 @@ import { Managers } from '@constants'
 import { createPreset } from '@lib'
 
 export default createPreset({
-  enabledManagers: [Managers.HELM]
+  enabledManagers: [Managers.HELM, Managers.HELM_VALUES]
 })
