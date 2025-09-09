20020a4f8ee64ca678181254a5d42c77d8f71674
Cenk Kılıç <cenk@kilic.dev>
2025-09-09 21:16:25 +0200

    fix: update defaults

--- src/presets/base.ts
+++ src/presets/base.ts
@@ -2,7 +2,17 @@ import { ASSIGNEES, TIMEZONE } from '@constants'
 import { createPreset } from '@lib'
 
 export default createPreset({
-  extends: ['config:recommended', ':configMigration', ':dependencyDashboard', ':dependencyDashboardApproval', ':masterIssue', ':disableRateLimiting'],
+  extends: [
+    'config:recommended',
+    ':configMigration',
+    ':dependencyDashboard',
+    ':separateMultipleMajorReleases',
+    ':masterIssue',
+    ':disableRateLimiting',
+    ':updateNotScheduled',
+    ':enableVulnerabilityAlerts',
+    ':approveMajorUpdates'
+  ],
   timezone: TIMEZONE,
   semanticCommits: 'enabled',
   assignees: ASSIGNEES,
