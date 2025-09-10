3012787626394981fe5613d1874770af5f14ce48
Cenk Kılıç <cenk@kilic.dev>
2025-09-11 01:14:26 +0200

    fix: preset

--- src/presets/managers/gitlab-ci/manager.ts
+++ src/presets/managers/gitlab-ci/manager.ts
@@ -1,8 +1,8 @@
 import { Managers } from '@constants'
-import { createPreset } from '@lib'
+import { createPreset, createScopes } from '@lib'
 import { Preset } from '@presets'
 
 export default createPreset({
   enabledManagers: [Managers.GITLAB_CI_INCLUDE, Managers.GITLAB_CI],
-  extends: [Preset.GITLAB_CI_MINOR_UPDATES]
+  extends: createScopes(Preset.GITLAB_CI_MINOR_UPDATES)
 })
