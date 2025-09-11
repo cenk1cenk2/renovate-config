d319fce75980d3ae7d3dff8853a63f1c2f33f6a5
Cenk Kılıç <cenk@kilic.dev>
2025-09-11 22:34:38 +0200

    WIP: [skip ci]
    Signed-off-by: Cenk Kılıç <cenk@kilic.dev>

--- src/presets/managers/terraform/custom-manager.ts
+++ src/presets/managers/terraform/custom-manager.ts
@@ -9,8 +9,10 @@ export default createPreset({
       customType: 'regex',
       managerFilePatterns: ['/\\.tf$/'],
       matchStringsStrategy: 'combination',
+      // source = "git::git@gitlab.kilic.dev:terraform/tf-modules.git//reloader?ref=reloader@1.0.6"
       matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/.*\?ref=(?<depName>.*)@(?<currentValue>[^"]+)"/.source],
       extractVersionTemplate: '^{{{depName}}}@(?<version>.*)$',
+      registryUrlTemplate: 'https://{{{registryUrl}}}',
       datasourceTemplate: 'gitlab-tags',
       versioningTemplate: 'semver'
     }
