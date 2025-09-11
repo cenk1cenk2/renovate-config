5583e666ae50eb5a8696431ea959fe8e54ef0b11
Cenk Kılıç <cenk@kilic.dev>
2025-09-11 02:11:12 +0200

    fix: update tags

--- src/presets/managers/gitlab-ci/custom-manager.ts
+++ src/presets/managers/gitlab-ci/custom-manager.ts
@@ -6,7 +6,7 @@ export default createPreset({
       customType: 'regex',
       managerFilePatterns: ['/\\.gitlab-ci\\.ya?ml$/'],
       matchStrings: ['project: [\'"](?<depName>[^/]+/[^/]+)[\'"]\n.*ref: (?<packageName>[^@]+)@(?<currentValue>[^"\\s]+)'],
-      datasourceTemplate: 'git-tags',
+      datasourceTemplate: 'gitlab-tags',
       registryUrlTemplate: 'https://gitlab.kilic.dev',
       packageNameTemplate: '{{{depName}}}',
       extractVersionTemplate: '^{{{packageName}}}@v?(?<version>.*)$'
