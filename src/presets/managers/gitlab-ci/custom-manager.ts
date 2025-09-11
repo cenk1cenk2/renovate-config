0dc71584327a12d63d967ed241e11bca5a930446
Cenk Kılıç <cenk@kilic.dev>
2025-09-11 21:57:54 +0200

    WIP:
    Signed-off-by: Cenk Kılıç <cenk@kilic.dev>

--- src/presets/managers/gitlab-ci/custom-manager.ts
+++ src/presets/managers/gitlab-ci/custom-manager.ts
@@ -3,14 +3,14 @@ import { createPreset } from '@lib'
 export default createPreset({
   customManagers: [
     {
+      depTypeTemplate: 'gitlab-ci-custom-manager-pipelines',
       customType: 'regex',
       managerFilePatterns: ['/\\.gitlab-ci\\.ya?ml$/'],
       matchStringsStrategy: 'combination',
       matchStrings: [/project:\s+['"]?(?<registryUrl>[^'"\s]+)['"]?\s*ref:\s+['"]?(?<packageName>[^@]+)@(?<currentValue>[^'"\s]+)['"]?/.source],
       registryUrlTemplate: 'https://gitlab.kilic.dev/{{{registryUrl}}}',
-      extractVersionTemplate: '^{{{packageName}}}@v?(?<version>.*)$',
-      datasourceTemplate: 'gitlab-tags',
-      versioningTemplate: 'gitlab-tags'
+      extractVersionTemplate: '^{{{packageName}}}@(?<version>.*)$',
+      datasourceTemplate: 'gitlab-tags'
     }
   ]
 })
