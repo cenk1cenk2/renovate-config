0e1d7fbf0eb9fa42e07dea27c0d1d3f1c7d768b4
Cenk Kılıç <cenk@kilic.dev>
2025-09-13 01:34:57 +0200

    fix: update cm

--- src/presets/managers/terraform/custom-manager.ts
+++ src/presets/managers/terraform/custom-manager.ts
@@ -22,7 +22,7 @@ export default createPreset({
       managerFilePatterns: ['/\\.tf$/'],
       matchStringsStrategy: 'any',
       // source = "git::git@gitlab.kilic.dev:terraform/tf-modules.git//reloader"
-      matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/(?<depName>.*)[^"]"/.source],
+      matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/(?<depName>[^"]+)"/.source],
       currentValueTemplate: '0.0.0',
       autoReplaceStringTemplate: '"git::git@{{{registryUrl}}}:{{{packageName}}}.git//{{{depName}}}?ref={{{depName}}}@{{{newVersion}}}"',
       extractVersionTemplate: '^{{{depName}}}@(?<version>.*)$',
