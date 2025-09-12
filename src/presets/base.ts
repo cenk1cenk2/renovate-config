dca3168fe5bef013f3869491708cb28c8fdfb77c
Cenk Kılıç <cenk@kilic.dev>
2025-09-13 01:41:38 +0200

    fix: update major rules

--- src/presets/base.ts
+++ src/presets/base.ts
@@ -2,17 +2,7 @@ import { ASSIGNEES, TIMEZONE } from '@constants'
 import { createPreset } from '@lib'
 
 export default createPreset({
-  extends: [
-    'config:recommended',
-    ':configMigration',
-    ':dependencyDashboard',
-    ':separateMultipleMajorReleases',
-    ':masterIssue',
-    ':disableRateLimiting',
-    ':updateNotScheduled',
-    ':enableVulnerabilityAlerts'
-    // ':approveMajorUpdates'
-  ],
+  extends: ['config:recommended', ':configMigration', ':dependencyDashboard', ':masterIssue', ':disableRateLimiting', ':updateNotScheduled', ':enableVulnerabilityAlerts'],
   timezone: TIMEZONE,
   semanticCommits: 'enabled',
   assignees: ASSIGNEES,
