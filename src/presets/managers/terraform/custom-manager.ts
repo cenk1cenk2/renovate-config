520f977473b34945a17fe25ccd22b44fb5383030
Cenk Kılıç <cenk@kilic.dev>
2025-09-13 02:33:56 +0200

    fix: rollback template

--- src/presets/managers/terraform/custom-manager.ts
+++ src/presets/managers/terraform/custom-manager.ts
@@ -24,8 +24,7 @@ export default createPreset({
       // source = "git::git@gitlab.kilic.dev:terraform/tf-modules.git//reloader"
       matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/(?<depName>[^"]+)"/.source],
       currentValueTemplate: '0.0.0',
-      // autoReplaceStringTemplate: '"git::git@{{{registryUrl}}}:{{{packageName}}}.git//{{{depName}}}?ref={{{depName}}}@{{{newVersion}}}"',
-      depNameTemplate: '{{{depName}}}?ref={{{depName}}}@{{{newVersion}}}',
+      autoReplaceStringTemplate: '"git::git@{{{registryUrl}}}:{{{packageName}}}.git//{{{depName}}}?ref={{{depName}}}@{{{newVersion}}}"',
       extractVersionTemplate: '^{{{depName}}}@(?<version>.*)$',
       registryUrlTemplate: 'https://{{{registryUrl}}}',
       datasourceTemplate: 'gitlab-tags',
