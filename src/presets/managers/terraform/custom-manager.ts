9507405fdfc7733f64d269f1452291519b562220
Cenk Kılıç <cenk@kilic.dev>
2025-09-14 00:19:49 +0200

    fix: update expression

--- src/presets/managers/terraform/custom-manager.ts
+++ src/presets/managers/terraform/custom-manager.ts
@@ -22,10 +22,9 @@ export default createPreset({
       managerFilePatterns: ['/\\.tf$/'],
       matchStringsStrategy: 'any',
       // source = "git::git@gitlab.kilic.dev:terraform/tf-modules.git//reloader"
-      matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/(?<depName>[^"]+)"/.source],
+      matchStrings: [/"git::git@(?<registryUrl>[^:]*):(?<packageName>[^.]*)(\.git)?\/\/(?<depName>[^?"]+)"/.source],
       currentValueTemplate: '0.0.0',
       autoReplaceStringTemplate: '"git::git@{{{registryUrl}}}:{{{packageName}}}.git//{{{depName}}}?ref={{{depName}}}@{{{newVersion}}}"',
-      depNameTemplate: '{{{ depName }}}',
       extractVersionTemplate: '^{{{depName}}}@(?<version>.*)$',
       registryUrlTemplate: 'https://{{{registryUrl}}}',
       datasourceTemplate: 'gitlab-tags',
