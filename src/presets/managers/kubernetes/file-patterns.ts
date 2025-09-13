03dc8fca99af980720af4ab35bdacd3e0ebeb92b
Cenk Kılıç <cenk@kilic.dev>
2025-09-13 03:06:27 +0200

    WIP: [skip ci]
    Signed-off-by: Cenk Kılıç <cenk@kilic.dev>

--- src/presets/managers/kubernetes/file-patterns.ts
+++ src/presets/managers/kubernetes/file-patterns.ts
@@ -4,15 +4,16 @@ import { createPreset } from '@lib'
 export default createPreset({
   packageRules: [
     {
+      enabled: true,
       matchManagers: [Managers.KUBERNETES],
       managerFilePatterns: [
-        '/^\\.deploy\\/**\\/*\\.ya?ml$/',
-        '/^!\\.deploy\\/**\\/kustomization\\.ya?ml$/',
-        '/^!\\.deploy\\/**\\/templates\\//',
-        '/^!\\.deploy\\/**\\/files\\//',
-        '/^!\\.deploy\\/**\\/values\\//',
-        '/^!\\.deploy\\/**\\/values\\.ya?ml$/',
-        '/^!\\.deploy\\/**\\/chart\\.ya?ml$/'
+        '/^\\.deploy\\/**\\/*\\.ya?ml$/'
+        // '/^!\\.deploy\\/**\\/kustomization\\.ya?ml$/',
+        // '/^!\\.deploy\\/**\\/templates\\//',
+        // '/^!\\.deploy\\/**\\/files\\//',
+        // '/^!\\.deploy\\/**\\/values\\//',
+        // '/^!\\.deploy\\/**\\/values\\.ya?ml$/',
+        // '/^!\\.deploy\\/**\\/chart\\.ya?ml$/'
       ]
     }
   ]
