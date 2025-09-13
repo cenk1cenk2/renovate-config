fe8a336d05bc2de464f030add37290a57d592eb5
Cenk Kılıç <cenk@kilic.dev>
2025-09-13 03:00:45 +0200

    fix: update preset

--- src/presets/managers/kubernetes/manager.ts
+++ src/presets/managers/kubernetes/manager.ts
@@ -4,12 +4,12 @@ import { createPreset } from '@lib'
 export default createPreset({
   enabledManagers: [Managers.KUBERNETES],
   managerFilePatterns: [
-    '/^\\.deploy\\/**\\/*\\.ya?ml$/',
-    '/^!\\.deploy\\/**\\/kustomization\\.ya?ml$/',
-    '/^!\\.deploy\\/**\\/templates\\//',
-    '/^!\\.deploy\\/**\\/files\\//',
-    '/^!\\.deploy\\/**\\/values\\//',
-    '/^!\\.deploy\\/**\\/values\\.ya?ml$/',
-    '/^!\\.deploy\\/**\\/chart\\.ya?ml$/'
+    '/^\\.deploy\\/**\\/*\\.ya?ml$/'
+    // '/^!\\.deploy\\/**\\/kustomization\\.ya?ml$/',
+    // '/^!\\.deploy\\/**\\/templates\\//',
+    // '/^!\\.deploy\\/**\\/files\\//',
+    // '/^!\\.deploy\\/**\\/values\\//',
+    // '/^!\\.deploy\\/**\\/values\\.ya?ml$/',
+    // '/^!\\.deploy\\/**\\/chart\\.ya?ml$/'
   ]
 })
