68eaea89a800193497f3fe8b361cd63479f0d008
Cenk Kılıç <cenk@kilic.dev>
2026-09-10 22:07:07 +0200

    fix: dont publish to npm

--- release.config.cjs
+++ release.config.cjs
@@ -1,10 +1,4 @@
 module.exports = {
   extends: '@cenk1cenk2/semantic-release-config',
-  plugins: [
-    [
-      '@cenk1cenk2/semantic-release-config/presets/npm',
-      { publish: 'staged', client: 'pnpm', assets: { extend: ['default.json'] } }
-    ],
-    '@semantic-release/gitlab'
-  ]
+  plugins: [['@cenk1cenk2/semantic-release-config/presets/tag', {}], '@semantic-release/gitlab']
 }
